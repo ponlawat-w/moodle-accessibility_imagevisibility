77fec781eb3daf653679de0b466355af71e6502e
ponlawat-w <ponlawat_w@outlook.co.th>
2024-12-01 14:45:30 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'accessibility_imagevisibility';
 $plugin->release = '1.0.0';
-$plugin->version = 2022041902;
+$plugin->version = 2022041903;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
